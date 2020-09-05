3d46dd155d7627741bdbb209c67f73540f3bb62a
Shmulik Braha <shmulik.b@sapiens.com>
2020-09-05 15:34:51 +0300

    Added Comments

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -6,13 +6,25 @@ using System.Web.Mvc;
 
 namespace MVCDemo.Controllers
 {
+    /// <summary>
+    /// Controller link between a View and the Webpage
+    /// HomeController means that under Views there will be a Home View which all Action Results are linked to
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// the View will be under Views -> Home -> Index
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// the View will be under Views -> Home -> About
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +32,10 @@ namespace MVCDemo.Controllers
             return View();
         }
 
+        /// <summary>
+        /// the View will be under Views -> Home -> Contact
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
